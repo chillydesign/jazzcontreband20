9c1b007edfbd17ff7e18835ce7078cf5a1ce1041
Charles Harvey <harvey.charles@gmail.com>
2020-05-25 22:32:07 +0200

    add white box to page

--- page.php
+++ page.php
@@ -1,35 +1,48 @@
 <?php get_header(); ?>
 
 
-	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-        <?php if ( is_front_page()  == false &&  $post->post_name != 'accueil' ) : ?>
-            <?php $image = (has_post_thumbnail())  ?  get_the_post_thumbnail_url() : false ?>
-            <?php get_template_part( 'partials/page-header' ); ?>
-        <?php endif; ?>
+		<?php if (is_front_page()  == false &&  $post->post_name != 'accueil') : ?>
+			<?php $image = (has_post_thumbnail())  ?  get_the_post_thumbnail_url() : false ?>
+			<?php get_template_part('partials/page-header'); ?>
+		<?php endif; ?>
 
 
 		<!-- article -->
-		<article id="post-<?php the_ID(); ?>" >
-			<?php if( !post_password_required( $post )): ?>
+		<article id="post-<?php the_ID(); ?>">
+			<?php if (!post_password_required($post)) : ?>
+
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-4">&nbsp;</div>
+						<div class="col-sm-8">
+							<div class="white_box">
+								<?php the_content(); ?>
+							</div>
+						</div>
+					</div>
+				</div>
+
+
 				<?php include('section-loop.php'); ?>
-				<?php if( have_rows('galleries') ) get_template_part('gallery_content'); ?>
-				<?php if( have_rows('press') ) get_template_part('press_content'); ?>
+				<?php if (have_rows('galleries')) get_template_part('gallery_content'); ?>
+				<?php if (have_rows('press')) get_template_part('press_content'); ?>
 			<?php endif; ?>
 
-			<?php the_content(); ?>
+
 
 		</article>
 		<!-- /article -->
 
 	<?php endwhile; ?>
 
-<?php else: ?>
+<?php else : ?>
 
 	<!-- article -->
 	<article class="container">
 
-		<h2><?php _e( 'Sorry, nothing to display.', 'webfactor' ); ?></h2>
+		<h2><?php _e('Sorry, nothing to display.', 'webfactor'); ?></h2>
 
 	</article>
 	<!-- /article -->
